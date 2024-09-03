73b7d0ed7f11930d08ca13dee9cca3fe9628624d
Oskar <oskw@itu.dk>
2024-09-03 15:16:08 +0200

    1.4 1-3 done, missing 4

---
+++ assignment1/objectoriented/Main.java
@@ -0,0 +1,139 @@
+import java.util.ArrayList;
+//1.4 I - IV here
+public class Main{
+    public static void main(String[] args){
+        Expr e1 = new Add(new CstI(17), new Var("z"));
+        System.out.println(e1.toString());
+        Expr e2 = new Mul(new Add(new CstI(3), new CstI(4)), new Var("name"));
+        System.out.println(e2.toString());
+        Expr e3 = new Sub(new Mul(new Var("var1"), new Var("var2")), new Mul(new CstI(3), new CstI(4)));
+        System.out.println(e3.toString());
+        Expr e4 = new Mul(new Sub(new CstI(3), new CstI(4)), new Add(new CstI(3), new CstI(4)));
+        System.out.println(e4.toString());
+
+        ArrayList<Tuple> env = new ArrayList<Tuple>();
+        env.add(new Tuple("z", 17));
+        env.add(new Tuple("name", 10));
+        env.add(new Tuple("var1", 10));
+        env.add(new Tuple("var2", 5));
+        try{
+            System.out.println(e1.eval(env));
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        try{
+            System.out.println(e2.eval(env));
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
+//not very pretty
+class Tuple{
+    public String x;
+    public int y;
+    public Tuple(String x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+abstract class Expr {
+    public abstract int eval(ArrayList<Tuple> env) throws Exception;
+    public abstract String toString();
+}
+class CstI extends Expr{
+    int value;
+    public CstI(int value){
+        this.value = value;
+    }
+    public String toString(){
+        return value + "";
+    }
+    public int eval(ArrayList<Tuple> env){
+        return value;
+    }
+}
+class Var extends Expr{
+    String varname;
+    public Var(String varname){
+        this.varname = varname;
+    }
+    public String toString(){
+        return varname + "";
+    }
+    public int eval(ArrayList<Tuple> env) throws Exception{
+        try{
+            return lookup(env, varname);
+        }
+        catch (Exception e){
+                throw e;
+        }
+    }
+    public int lookup(ArrayList<Tuple> env, String var) throws Exception{
+        for (Tuple tuple : env){
+            if (tuple.x.equals(var)){
+                return tuple.y;
+            }
+        }
+        throw new Exception("variable does not exist in environment");
+    }
+}
+abstract class Binop extends Expr{
+    
+}
+class Add extends Binop{
+    Expr expr1, expr2;
+    public Add(Expr expr1, Expr expr2){
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+    }
+    public String toString(){
+        return "("+ expr1.toString() + " + " + expr2.toString() + ")"; 
+    }
+    public int eval(ArrayList<Tuple> env) throws Exception{
+        try {
+            return expr1.eval(env) + expr2.eval(env);
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
+}
+class Sub extends Binop{
+    Expr expr1, expr2;
+    public Sub(Expr expr1, Expr expr2){
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+    }
+    public String toString(){
+        return "(" + expr1.toString() + " - " + expr2.toString() + ")";
+    }
+    public int eval(ArrayList<Tuple> env) throws Exception{
+        try {
+            return expr1.eval(env) - expr2.eval(env);
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
+}
+class Mul extends Binop{
+    Expr expr1, expr2;
+    public Mul(Expr expr1, Expr expr2){
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+    }
+    public String toString(){
+        return "(" + expr1.toString() + " * " + expr2.toString() + ")";
+    }
+    public int eval(ArrayList<Tuple> env) throws Exception{
+        try {
+            return expr1.eval(env) * expr2.eval(env);
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
+}
