82fe1ebe32b97cbc4be0be28c16dff057f10b97f
Oskar <oskw@itu.dk>
2024-09-26 10:56:42 +0200

    422

--- assignment4/Answers.fs
+++ assignment4/Answers.fs
@@ -4,3 +4,5 @@ open Parse
 open ParseAndRun
 
 let e421 = fromString "let sum n = n + if (n=0) then 0 else (sum (n-1)) in sum 1000 end";;
+
+let e422 = fromString "let treer n = 3 * (if n = 0 then 1 else 3 * (treer (n - 1))) in treer 8"
