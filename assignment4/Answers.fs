0d658086df814839352c66b0ce6ede1dbb2d4ac5
avad <avad@itu.dk>
2024-09-26 11:31:43 +0200

    4.2 done

--- assignment4/Answers.fs
+++ assignment4/Answers.fs
@@ -5,4 +5,12 @@ open ParseAndRun
 
 let e421 = fromString "let sum n = n + if (n=0) then 0 else (sum (n-1)) in sum 1000 end";;
 
-let e422 = fromString "let treer n = 3 * (if n = 0 then 1 else 3 * (treer (n - 1))) in treer 8"
+let e422 = fromString "let treer n = (if n = 0 then 1 else  3 * (treer (n - 1))) in treer 8 end";;
+
+let e423 = fromString "let treer n = (if n = 0 then 1 else  3 * (treer (n - 1))) in 
+        let treesum n = treer n + if (n=0) then 0 else (treesum (n-1)) in 
+        treesum 11 end end";;
+
+let e424 = fromString "let pow8 n = n*n*n*n*n*n*n*n in 
+        let powsum n = pow8 n + if (n=0) then 0 else (powsum (n-1)) in 
+        powsum 10 end end";;
