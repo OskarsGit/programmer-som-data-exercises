df376b74d900cbef4c4deb585713cb7a27aa72f3
Oskar <oskw@itu.dk>
2024-10-24 11:03:50 +0200

    exercuse72ii done?

--- assignment6/MicroC/exercise72ii.c
+++ assignment6/MicroC/exercise72ii.c
@@ -1,8 +1,26 @@
 void main(int n) {
-  int[] arr[20];
-    
+    int[] arr[20];
+    int printer;
+    printer = 0;
+    squares(n, arr);
+    arrsum(n, arr, &printer);
+
+    print printer;
 }
 
-void squares(int n, int arrs[]) {
-    
+void squares(int n, int arr[]) {
+    int i;
+    i = 0;
+    while (i < n){
+        arr[i] = i * i;
+    }
+}
+
+void arrsum(int n, int arr[], int *sump) {
+    int i;
+    i = 0;
+    while (i < n){
+        *sump = *sump + arr[i];
+        i = i + 1;
+    }
 }
