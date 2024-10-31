7e46d88621cce0049777e38cb35f3360012354c3
avad <avad@itu.dk>
2024-10-31 17:15:28 +0100

    8.6 more

--- assignment7/Comp.fs
+++ assignment7/Comp.fs
@@ -144,6 +144,7 @@ let rec cStmt stmt (varEnv : varEnv) (funEnv : funEnv) : instr list =
       [RET (snd varEnv - 1)]
     | Return (Some e) -> 
       cExpr e varEnv funEnv @ [RET (snd varEnv)]
+    | Switch (e1, es) -> failwith "failed"
 
 and cStmtOrDec stmtOrDec (varEnv : varEnv) (funEnv : funEnv) : varEnv * instr list = 
     match stmtOrDec with 
